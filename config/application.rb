8a382bacd0b9a44d24d2fac1a1a7252d811c007a
twillis <twillis@relevantinc.com>
2019-07-25 10:21:13 -0600

    Fixed secret key

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ Bundler.require(*Rails.groups)
 module MyApp
   class Application < Rails::Application
   	config.eager_load_paths << "#{Rails.root}/lib"
-  	config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end   
 end
